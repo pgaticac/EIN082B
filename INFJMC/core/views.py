e400221af404ef739fae174e25a4e01290547925
Pame Gatica <pamela.gatica@usm.cl>
2024-04-29 14:35:40 -0400

    Semana 8 - Templates y Statics

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -2,13 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-   # return HttpResponse('<h1>Hola Mundo</h1>')
-    return render(request, 'core/home.html')
+    title = "Inicio"
+
+    data = {
+        "title" : title,
+    }
+
+    return render(request, 'core/home.html',data)
+
+
 def carreras(request):
     #return HttpResponse('<h1>Carreras</h1>')
-    return render(request, 'core/carreras.html')
+    title = "Carreras"
+
+    carrerasList =[
+        "Técnico Universitario en Informática",
+        "Ingeniería en Informática",
+        "Ingeniería de Ejecución en Software",
+    ]
+
+    data = {
+        "title":title,
+        "carreras":carrerasList
+    }
+    return render(request, 'core/carreras.html',data)
 
 
 def docentes(request):
     #return HttpResponse('<h1>Docentes</h1>')
-    return render(request, 'core/docentes.html')
+    title = "Profesores"
+
+    data = {
+        "title":title,
+    }
+    return render(request, 'core/docentes.html',data)
