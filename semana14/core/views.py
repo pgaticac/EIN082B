9c8dda0e90f54a05446909f461d70858278e7753
Pame Gatica <pamela.gatica@usm.cl>
2024-11-04 14:25:52 -0300

    Semana 14 - Init

---
+++ semana14/core/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Carrera
+
+# Create your views here.
+def home(request):
+    titulo = "Inicio"
+    data = {
+        "titulo": titulo
+    }
+    return render(request,'core/index.html', data)
+
+def carreras(request):
+    titulo = "Carreras"
+        
+    if (request.POST):
+        #capturar los datos desde el form
+        codigo = request.POST["codigo"]
+        nombre = request.POST["nombre"]
+        duracion = request.POST["duracion"]
+        
+        #Validaciones
+        
+        
+        
+        
+        nueva = Carrera(codigo=codigo,nombre=nombre,duracion=duracion)
+        nueva.save()    
+    
+    carreras = Carrera.objects.all()
+    data = {
+        "titulo":titulo,
+        "carreras": carreras
+    }
+    return render(request,'core/carreras.html',data)
+    
+
+def profesores(request):
+    titulo = "Profesores"
+    data = {
+        "titulo":titulo,
+    
+    }
+    return render(request,'core/profesores.html',data)
